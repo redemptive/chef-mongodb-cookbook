dc9e2c6e4dfdb9711aca44af32eb495ee0780485
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2018-02-06 11:46:07 +0000

    Finished cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,35 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+apt_update
+
+apt_repository 'mongodb' do
+  uri          'http://repo.mongodb.org/apt/ubuntu'
+  distribution 'xenial/mongodb-org/3.2'
+  key 			'EA312927'
+  keyserver 	'hkp://keyserver.ubuntu.com:80'
+  components   ['multiverse']
+end
+
+package 'mongodb-org' do
+	action :upgrade
+end
+
+template '/lib/systemd/system/mongod.service' do
+	source 'mongod.service.erb'
+	owner 'mongodb'
+	group 'mongodb'
+	mode '0750'
+end
+
+template '/etc/mongod.conf' do
+	source 'mongod.conf.erb'
+	owner 'mongodb'
+	group 'mongodb'
+	mode '0750'
+end
+
+service 'mongod' do
+	action [:enable, :start]
+end
